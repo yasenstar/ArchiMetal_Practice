8a37abbc726f7110f9a2c7cef927aa90e6196cd2
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-05-19 07:53:33 -0400

    add mindmap about ArchiMetal structure

---
+++ ArchiMetal-Views-Structure.mm
@@ -0,0 +1,126 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="ArchiMetal" FOLDED="false" ID="ID_1090958577" CREATED="1606664858024" MODIFIED="1684495713469" VGAP_QUANTITY="3 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#fbf1c7">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_gruvbox_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_7974122" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#3c3836" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_7974122" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#cc241d" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#cc241d"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_103960811" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc241d" TRANSPARENCY="255" DESTINATION="ID_103960811"/>
+<font SIZE="12" ITALIC="false"/>
+<edge COLOR="#cc241d"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#fdf6e3" BACKGROUND_COLOR="#282828" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#282828" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#282828" BACKGROUND_COLOR="#ebdbb2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#282828" BACKGROUND_COLOR="#d79921" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#458588" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#ffffff" BACKGROUND_COLOR="#b16286" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#ffffff" BACKGROUND_COLOR="#689d6a" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#ffffff" BACKGROUND_COLOR="#a89984" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#ebdbb2" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#ebdbb2">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#98971a">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="1. Introduction" POSITION="right" ID="ID_504371281" CREATED="1684495764094" MODIFIED="1684495794814"/>
+<node TEXT="2. ArchiMetal Transformation Challenges (7 views)" POSITION="right" ID="ID_1913450260" CREATED="1684495774629" MODIFIED="1684497179993">
+<node TEXT="Figure 01: ArchiMetal Business Units" ID="ID_981986382" CREATED="1684495909402" MODIFIED="1684495926787"/>
+<node TEXT="Figure 02: Value Stream of ArchiMetal" ID="ID_436555127" CREATED="1684495931626" MODIFIED="1684495945220"/>
+<node TEXT="Figure 03: Production and Logistics of ArchiMetal" ID="ID_1809969619" CREATED="1684495953139" MODIFIED="1684495964747"/>
+<node TEXT="Figure 04: Steel Production" ID="ID_255245798" CREATED="1684495969386" MODIFIED="1684495977213"/>
+<node TEXT="Figure 05: Hot Strip Mill in More Detail" ID="ID_1939758981" CREATED="1684495981698" MODIFIED="1684495991787"/>
+<node TEXT="Figure 06: ArchiMate Motivation View Describing the Overall Challenges Facing ArchiMetal" ID="ID_1231707002" CREATED="1684495999811" MODIFIED="1684496025348"/>
+<node TEXT="Figure 07: ArchiMate Motivation View Describing the Customer Satisfaction and Service Quality Challenges Facing ArchiMetal" ID="ID_1978665699" CREATED="1684496032753" MODIFIED="1684496061741"/>
+</node>
+<node TEXT="3. ArchiMetal&apos;s CRM Vision (1 view)" POSITION="right" ID="ID_869228877" CREATED="1684495795717" MODIFIED="1684497167678">
+<node TEXT="Figure 08: ArchiMetal&apos;s CRM Vision" ID="ID_1687302338" CREATED="1684496070937" MODIFIED="1684496083612"/>
+</node>
+<node TEXT="4. ArchiMetal Transformation Overview (12 views)" POSITION="right" ID="ID_1160554472" CREATED="1684495802907" MODIFIED="1684497160681">
+<node TEXT="Figure 09: Customer Services Provided by DC Benelux" ID="ID_448668011" CREATED="1684496093994" MODIFIED="1684496113615"/>
+<node TEXT="Figure 10: Inter-Dependencies between the Key Sales Processes in DC Benelux, PC, and HQ" ID="ID_1283094661" CREATED="1684496119697" MODIFIED="1684496140619"/>
+<node TEXT="Figure 11: Most Important Information Flows between the Units" ID="ID_1472376792" CREATED="1684496146747" MODIFIED="1684496162571"/>
+<node TEXT="Figure 12: Application Landscape of ArchiMetal" ID="ID_1111695950" CREATED="1684496166587" MODIFIED="1684496179626"/>
+<node TEXT="Figure 13: Key Aspects of the ArchiMetal Corporate Data Center Infrastructure" ID="ID_657880318" CREATED="1684496184385" MODIFIED="1684496199794"/>
+<node TEXT="Figure 14: Changes to ArchiMetal Organization Structure, including New Department: Customer Service" ID="ID_1927986515" CREATED="1684496344217" MODIFIED="1684496367229"/>
+<node TEXT="Figure 15: Relevant Information Flows between Business Units" ID="ID_1852554709" CREATED="1684496373385" MODIFIED="1684496385451"/>
+<node TEXT="Figure 16: Overview of the Key Changes in the Overall Enterprise Architecture of ArchiMetal" ID="ID_183065244" CREATED="1684496390361" MODIFIED="1684496440588"/>
+<node TEXT="Figure 17: New Customer Service Enables Real-Time Monitoring of Customer Orders" ID="ID_39430074" CREATED="1684496450778" MODIFIED="1684496475185"/>
+<node TEXT="Figure 18: New Processes and their Inter-Dependencies with Existing Processes" ID="ID_740527289" CREATED="1684496480713" MODIFIED="1684496495755"/>
+<node TEXT="Figure 19: New Application Landscape with the CRM System Sharing Centralized Customer Data through an EAI Bus" ID="ID_1280815311" CREATED="1684496500602" MODIFIED="1684496732193"/>
+<node TEXT="Figure 20: Implementation and Migration View for ArchiMetal CRM Transformation Program" ID="ID_1649406198" CREATED="1684496737245" MODIFIED="1684496756518"/>
+</node>
+<node TEXT="5. Detailed Target Enterprise Architecture (7 views)" POSITION="right" ID="ID_1061483686" CREATED="1684495816667" MODIFIED="1684497146745">
+<node TEXT="Figure 21: Registration of a New Customer" ID="ID_1381879217" CREATED="1684496763701" MODIFIED="1684496773253"/>
+<node TEXT="Figure 22: Support for the Develop Contract Process" ID="ID_1291952068" CREATED="1684496779006" MODIFIED="1684496790629"/>
+<node TEXT="Figure 23: Registration of Customer Order" ID="ID_490864123" CREATED="1684496802675" MODIFIED="1684496879592"/>
+<node TEXT="Figure 24: Application Usage by Register Order Process" ID="ID_1817621283" CREATED="1684496884246" MODIFIED="1684496895088"/>
+<node TEXT="Figure 25: Track Customer Order Process" ID="ID_162035459" CREATED="1684496901047" MODIFIED="1684496910863"/>
+<node TEXT="Figure 26: Process Customer Order Process" ID="ID_890983830" CREATED="1684496924296" MODIFIED="1684496938936"/>
+<node TEXT="Figure 27: Applications Usage by the Process Customer Order Process" ID="ID_356952084" CREATED="1684496941590" MODIFIED="1684496958593"/>
+</node>
+<node TEXT="6. Target State Scenario: Processing an Urgent Customer Order (5 views)" POSITION="right" ID="ID_1108094041" CREATED="1684495827611" MODIFIED="1684497134488">
+<node TEXT="Figure 28: Stakeholder View" ID="ID_1142834641" CREATED="1684496968894" MODIFIED="1684496975470"/>
+<node TEXT="Figure 29: Business Process Cooperation - Converting an Order to a Contract" ID="ID_1938810767" CREATED="1684497026790" MODIFIED="1684497045329"/>
+<node TEXT="Figure 30: Business Process Cooperation - Fulfillment of an Order at ArchiMetal" ID="ID_1838006880" CREATED="1684497052638" MODIFIED="1684497090159"/>
+<node TEXT="Figure 31: Business Process View" ID="ID_1002573362" CREATED="1684497094773" MODIFIED="1684497104048"/>
+<node TEXT="Figure 32: Information Structure View" ID="ID_1166452118" CREATED="1684497108343" MODIFIED="1684497116512"/>
+</node>
+<node TEXT="7. Benefits of Enterprise Architecture with the ArchiMate Language" POSITION="right" ID="ID_1870242932" CREATED="1684495841874" MODIFIED="1684495855645"/>
+<node TEXT="8. Conclusion" POSITION="right" ID="ID_1463392204" CREATED="1684495857466" MODIFIED="1684495861549"/>
+<node TEXT="9. Opportunities for Further Work" POSITION="right" ID="ID_1852173085" CREATED="1684495861730" MODIFIED="1684495869956"/>
+</node>
+</map>
